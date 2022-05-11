e27b43f74f1a4f8f08bc1bd665b9fb6b0e65b63f
RpaBest <parserdata00@gmail.com>
2022-05-11 16:33:59 +0300

    Release 0.1

---
+++ electroneek_hh/execute_js_code_scripts/set_config_1.js
@@ -0,0 +1,5 @@
+const today = new Date();
+const date = today.getFullYear()+'_'+(today.getMonth()+1)+'_'+today.getDate();
+const time = today.getHours() + "_" + today.getMinutes() + "_" + today.getSeconds();
+
+date_time = date+'___'+time;
