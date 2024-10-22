6eb83a365c52e40f7f2dbca304057f5fe039e35f
x.karimjonov <khtkarimzhonov@gmail.com>
2024-10-22 14:15:53 +0500

    fixes

--- main/hh_ru/api.py
+++ main/hh_ru/api.py
@@ -77,7 +77,7 @@ def collect_file(data):
     if not dfs:
         raise HTTPException(status_code=403, detail="Ваканции не найденo")
     df = pd.concat(dfs)
-    df.reset_index(inplace=True)
+    df.reset_index()
     df = df_style(df)
     output = io.BytesIO()
     writer = pd.ExcelWriter(output)
